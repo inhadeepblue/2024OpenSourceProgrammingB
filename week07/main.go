b89471da02091cff8272cab5b4de632574f6a30d
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 18:25:58 +0900

    v1.3 ParseInt, Base16

--- week07/main.go
+++ week07/main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	var realScore float64
 	fmt.Print("점수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	score, err := in.ReadString('\n')
@@ -19,11 +18,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	score = strings.TrimSpace(score)             // 줄바꿈, 띄어쓰기, 탭 등 제거 (python strip과 유사)
-	realScore, _ = strconv.ParseFloat(score, 64) // 실수형 64비트 타입으로 형변환
-	if realScore >= 90 {
+	score = strings.TrimSpace(score)                // 줄바꿈, 띄어쓰기, 탭 등 제거 (python strip과 유사)
+	realScore, _ := strconv.ParseInt(score, 16, 32) // 정수형 32비트 타입으로 형변환
+	if realScore >= 60 {
 		fmt.Println("A")
+		fmt.Printf("%d\n", realScore)
 	} else {
 		fmt.Println("BCDF")
+		fmt.Printf("%d\n", realScore)
 	}
 }
