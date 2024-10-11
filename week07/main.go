bb4da7e4c54b0772725deba7c076b765939aa93c
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 17:53:29 +0900

    v1.1 Shadowing

--- week07/main.go
+++ week07/main.go
@@ -1,20 +1,14 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	fmt.Print("이름 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	name, err := in.ReadString('\n')
+	// Shadowing problem
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println(name)
-	}
+	// var float32 float32 = 9.1
+	// fmt.Println(float32)
+
+	// var number float32
+	// fmt.Println(number)
+
+	// var fmt float32 = 9.1
+	// fmt.Println(float32)
 }
