d4fc17a6b4754e6e2b58829c9608dfdd05173cd5
inhadeepblue <deepblue@inha.ac.kr>
2024-10-25 17:19:20 +0900

    v1.9 Guess game

--- week09/main.go
+++ week09/main.go
@@ -14,8 +14,10 @@ import (
 func main() {
 	rand.Seed(time.Now().Unix())
 	answer := rand.Intn(6) + 1 // dice 1 ~ 6
-	fmt.Println(answer)
+	//fmt.Println(answer)
 
+	var win bool
+	win = false
 	for guesses := 0; guesses < 3; guesses++ {
 		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
 		in := bufio.NewReader(os.Stdin)
@@ -40,5 +42,11 @@ func main() {
 		} else { // answer < guess
 			fmt.Println("입력하신 값은 정답보다 큰 수 입니다. HIGH")
 		}
+	}
+
+	if win {
+		fmt.Println("당신이 이겼습니다!")
+	} else {
+		fmt.Printf("당신이 졌습니다. 정답은 %d입니다\n", answer)
 	}
 }
