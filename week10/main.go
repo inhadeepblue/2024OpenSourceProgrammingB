5d62b71f9e8ee682cff39a5e6d6ca0cb52ffa355
inhadeepblue <deepblue@inha.ac.kr>
2024-11-01 16:54:46 +0900

    v2.2 Prime number

---
+++ week10/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("정수 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	number, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	number = strings.TrimSpace(number)
+	n, err := strconv.Atoi(number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	counts := 0
+	i := 1
+	for i <= n {
+		if n%i == 0 {
+			counts = counts + 1
+		}
+		i++
+	}
+	if counts == 2 {
+		fmt.Printf("%d는(은) 소수입니다.", n)
+	} else {
+		fmt.Printf("%d는(은) 소수가 아닙니다!", n)
+	}
+}
