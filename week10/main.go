a3ddf0b5a48710f40f89bb6965ab8b2cb25cbb36
inhadeepblue <deepblue@inha.ac.kr>
2024-11-01 17:59:06 +0900

    v2.7 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	//fmt.Printf("%f\n", math.Sqrt(25.0))
 	fmt.Print("정수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	number, err := in.ReadString('\n')
@@ -28,7 +30,8 @@ func main() {
 		isPrime = false
 	} else {
 		i := 2
-		for i < n {
+		//for i < n {
+		for i <= int(math.Sqrt(float64(n))) {
 			if n%i == 0 {
 				isPrime = false
 				break // 1과 자기자신을 제외한 첫 번째 약수가 발견 되면 반복문 종료
